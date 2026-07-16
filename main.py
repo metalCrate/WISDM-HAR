6dea97c947f9f4d187e731e419ed0704ed951df1
metalCrate <240234085+metalCrate@users.noreply.github.com>
2026-07-16 11:01:37 +0200

    Added additional argument to main.py to allow for training the model.

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--preprocess', type=bool, default=False, help='Whether to preprocess the data, and generate splits')
+    parser.add_argument('--train', type=bool, default=False, help='Whether to train the model')
     args = parser.parse_args()
 
     if args.preprocess:
         # Preprocess the data
         generate_split_data()
 
-    # Train the model
-    train_from_config() 
+    if args.train:
+        # Train the model
+        train_from_config()
 
     # Evaluate the model
     test_model() 
